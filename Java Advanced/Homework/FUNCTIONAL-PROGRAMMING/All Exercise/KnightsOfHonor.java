d72b0f0b4c511c270d08f00fb36569ec48a3a688
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-01-25 22:19:50 +0200

    Add Exercise

---
+++ Java Advanced/Homework/FUNCTIONAL-PROGRAMMING/All Exercise/KnightsOfHonor.java
@@ -0,0 +1,16 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class KnightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Consumer<String> print = e -> System.out.printf("Sir %s\n", e);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .forEach(print);
+    }
+}
