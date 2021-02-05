d6e2dd2fd6fdfdac850a7c18c7fe5e817735c024
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-02-05 21:34:21 +0200

    Add Exercise

---
+++ Java Advanced/Homework/DEFINING-CLASSES/All Exercise/CompanyRoster/Employee.java
@@ -0,0 +1,51 @@
+package DefiningClasses.CompanyRoster;
+
+public class Employee {
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    String name;
+
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+        this.age = age;
+    }
+
+    double salary;
+    String position;
+    String department;
+    String email;
+    int age;
+
+
+}
