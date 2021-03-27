8ddde01003d340a3da1758fe05b3276892eeb7ba
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-03-27 22:47:17 +0200

    Add Exercise

---
+++ Java OOP/INHERITANCE/Zoo/Animal.java
@@ -0,0 +1,17 @@
+package Inheritance.Zoo;
+
+public class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+
+}
