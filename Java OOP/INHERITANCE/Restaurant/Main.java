2c76ad94f6a9a29ee8cbc31c282384117c8b8028
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-04-01 22:51:58 +0300

    Add Exercise

---
+++ Java OOP/INHERITANCE/Restaurant/Main.java
@@ -0,0 +1,13 @@
+package Inheritance.Restaurant;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      Coffee coffee = new Coffee("Lavazza", 1.2);
+
+        System.out.println(coffee);
+    }
+}
