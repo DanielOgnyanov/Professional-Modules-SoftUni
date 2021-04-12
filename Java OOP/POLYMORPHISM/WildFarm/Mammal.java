a894ea85872e9493e5d21057864186f09159a96c
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-04-12 21:39:00 +0300

    Add Exercise

---
+++ Java OOP/POLYMORPHISM/WildFarm/Mammal.java
@@ -0,0 +1,9 @@
+package Polymorphism.WildFarm;
+
+public abstract  class Mammal extends Animal{
+
+
+    protected Mammal(String animalName, String animalType, double weight, String livingReligion) {
+        super(animalName, animalType, weight, livingReligion);
+    }
+}
