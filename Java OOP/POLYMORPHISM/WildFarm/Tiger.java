882952a57f55198539aec1fbcef7277ab1098394
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-04-12 21:39:46 +0300

    Add Exercise

---
+++ Java OOP/POLYMORPHISM/WildFarm/Tiger.java
@@ -0,0 +1,20 @@
+package Polymorphism.WildFarm;
+
+public class Tiger extends Felime {
+    protected Tiger(String animalName, String animalType, double weight, String livingReligion) {
+        super(animalName, animalType, weight, livingReligion);
+    }
+
+    @Override
+    public void eat(Food food) {
+        if (food instanceof Vegetable){
+            throw new IllegalArgumentException("Tigers are not eating that type of food!");
+        }
+        super.eat(food);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("ROAAR!!!");
+    }
+}
