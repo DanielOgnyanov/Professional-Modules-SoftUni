ad7c82a82372f3061354c43272f9d7600bfcf57a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-04-12 21:38:15 +0300

    Add Exercise

---
+++ Java OOP/POLYMORPHISM/WildFarm/Felime.java
@@ -0,0 +1,7 @@
+package Polymorphism.WildFarm;
+
+public  abstract  class Felime extends Mammal {
+    protected Felime(String animalName, String animalType, double weight, String livingReligion) {
+        super(animalName, animalType, weight, livingReligion);
+    }
+}
