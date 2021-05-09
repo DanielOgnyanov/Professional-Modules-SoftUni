6f2a7f940aa0cb68287319a3a2b97de83cfc0ca5
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-05-09 21:28:00 +0300

    SQL  Query

---
+++ My SQL/Data Definition and Data Types/Create Table People.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `people` (
+	`id` int NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+	`name` VARCHAR(200) NOT NULL,
+	`picture` BLOB,
+	`height` DOUBLE(3,2),
+	`weight` DOUBLE(5,2),
+	`gender` ENUM('m', 'f') NOT NULL,
+	`birthdate` date NOT NULL,
+	`biography` LONGTEXT
+) ENGINE=InnoDB;
+
+INSERT INTO `people`(`name`, `gender`, `birthdate`)
+	VALUES 
+		('Pesho', 'm', '1980-02-10'),
+		('Gosho', 1, '1985-10-15'),
+		('Maria', 'f', '1985-10-15'),
+		('Pena', 2, '1986-10-15'),
+		('Tosho', 1, '1976-10-15');
