06445305470beab4ff5f273a1221144cb98b6838
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-07-19 22:20:58 +0300

    SQL Query

--- Spring Data/ALL-EXAM/Instagraph Lite/skeleton/src/main/java/softuni/exam/instagraphlite/util/XmlParser.java
+++
@@ -1,7 +0,0 @@
-package softuni.exam.instagraphlite.util;
-
-import javax.xml.bind.JAXBException;
-
-public interface XmlParser {
-    <O> O parseXml(Class<O> objectClass, String filePath) throws JAXBException;
-}
