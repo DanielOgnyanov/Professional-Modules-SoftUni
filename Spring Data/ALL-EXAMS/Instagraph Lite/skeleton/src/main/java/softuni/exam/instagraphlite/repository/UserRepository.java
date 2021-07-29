f6398cd41eef9b6209b6ba0f8c0e5a735ce69123
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-07-29 19:21:03 +0300

    Update Query

--- Spring Data/ALL-EXAMS/Instagraph Lite/skeleton/src/main/java/softuni/exam/instagraphlite/repository/UserRepository.java
+++ Spring Data/ALL-EXAMS/Instagraph Lite/skeleton/src/main/java/softuni/exam/instagraphlite/repository/UserRepository.java
@@ -9,4 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
     Optional<Users> findByUsername(String username);
+    
+    @Query("SELECT DISTINCT u FROM Users u JOIN FETCH u.posts p ORDER BY size(p) DESC, u.id")
+    List<Users> result();
 }
