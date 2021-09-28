2f8d80122ecc91858e2bc3851a1443ddd775b558
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-09-28 18:57:10 +0300

    Enum category set value

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/entities/EnumCategory.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/entities/EnumCategory.java
@@ -1,9 +1,19 @@
 package com.example.coffee_shop_project.models.entities;
 
 public enum EnumCategory {
-    COFFEE,
-    CAKE,
-    DRINK,
-    OTHER;
+    COFFEE (2),
+    CAKE (10),
+    DRINK (1),
+    OTHER (5);
 
+
+    private int value;
+
+    EnumCategory(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
