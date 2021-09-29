6ed150928dbda688c6e9efdfa7db9f366dd5a099
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-09-29 18:15:25 +0300

    Database init

---
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/init/DatabaseInit.java
@@ -0,0 +1,19 @@
+package com.example.coffee_shop_project.init;
+
+import com.example.coffee_shop_project.service.CategoryService;
+import org.springframework.boot.CommandLineRunner;
+
+public class DatabaseInit implements CommandLineRunner {
+
+    private  final CategoryService categoryService;
+
+    public DatabaseInit(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        categoryService.initCategories();
+    }
+}
