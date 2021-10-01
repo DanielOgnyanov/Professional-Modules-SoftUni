d158fffc3f3771c7f169498ab964833edd831e81
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-01 19:00:05 +0300

    User service

---
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.coffee_shop_project.service.Impl;
+
+import com.example.coffee_shop_project.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
