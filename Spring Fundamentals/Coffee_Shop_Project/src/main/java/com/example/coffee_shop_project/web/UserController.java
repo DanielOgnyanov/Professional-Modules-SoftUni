b734e91a2f55885281c8291e10be96220521df8a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-02 16:23:50 +0300

    UserController @Valid BidingModel

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/UserController.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/UserController.java
@@ -1,8 +1,14 @@
 package com.example.coffee_shop_project.web;
 
+import com.example.coffee_shop_project.models.biding.UserRegisterBidingModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -14,5 +20,12 @@ public class UserController {
         return "register";
     }
 
+
+    @PostMapping("/register")
+    public String registerConfirm(@Valid UserRegisterBidingModel userRegisterBidingModel,
+                                  BindingResult bindingResult,
+                                  RedirectAttributes redirectAttributes){
+
+    }
 
 }
