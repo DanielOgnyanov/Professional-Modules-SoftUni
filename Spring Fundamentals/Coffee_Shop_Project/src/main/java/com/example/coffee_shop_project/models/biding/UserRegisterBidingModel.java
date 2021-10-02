ed61d51c038f3eafb423cb0fb54f0dac1e0578de
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-02 16:05:53 +0300

    User BidingModel setting

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/biding/UserRegisterBidingModel.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/biding/UserRegisterBidingModel.java
@@ -1,4 +1,15 @@
 package com.example.coffee_shop_project.models.biding;
 
 public class UserRegisterBidingModel {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+
+
+
 }
