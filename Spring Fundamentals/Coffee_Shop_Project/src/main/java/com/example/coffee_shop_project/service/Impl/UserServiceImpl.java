59285503a6b77e32c6a7a7be5d5ee7c4f48ca6f4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-03 15:56:56 +0300

    User service insert in db set.

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/UserServiceImpl.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/UserServiceImpl.java
@@ -1,21 +1,26 @@
 package com.example.coffee_shop_project.service.Impl;
 
+import com.example.coffee_shop_project.models.entities.User;
 import com.example.coffee_shop_project.models.service.UserServiceModel;
 import com.example.coffee_shop_project.repository.UserRepository;
 import com.example.coffee_shop_project.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public void register(UserServiceModel userServiceModel) {
 
+        userRepository.save(modelMapper.map(userServiceModel, User.class));
     }
 }
