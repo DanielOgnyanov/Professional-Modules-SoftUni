ffd082b7c22ebd465b69a608bfccbe7b1185448f
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-03 15:55:02 +0300

    User service registration method set

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/UserServiceImpl.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.coffee_shop_project.service.Impl;
 
 import com.example.coffee_shop_project.models.service.UserServiceModel;
+import com.example.coffee_shop_project.repository.UserRepository;
 import com.example.coffee_shop_project.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public void register(UserServiceModel userServiceModel) {
