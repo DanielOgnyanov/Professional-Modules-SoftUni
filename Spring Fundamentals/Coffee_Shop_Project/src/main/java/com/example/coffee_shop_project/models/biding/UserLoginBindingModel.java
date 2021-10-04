0812b879dfaf42bf5a06323530d870bcdd83dc15
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-04 19:06:08 +0300

    User login binding model setting field validation

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/biding/UserLoginBindingModel.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/biding/UserLoginBindingModel.java
@@ -1,5 +1,8 @@
 package com.example.coffee_shop_project.models.biding;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class UserLoginBindingModel {
 
     private String username;
@@ -9,6 +12,8 @@ public class UserLoginBindingModel {
     public UserLoginBindingModel() {
     }
 
+    @Size(min = 5, max = 20 , message = "Username must be between 5 and 20 character")
+    @NotBlank
     public String getUsername() {
         return username;
     }
@@ -17,6 +22,8 @@ public class UserLoginBindingModel {
         this.username = username;
     }
 
+    @Size(min = 3, message = "Password size must be atleast 3 character")
+    @NotBlank
     public String getPassword() {
         return password;
     }
