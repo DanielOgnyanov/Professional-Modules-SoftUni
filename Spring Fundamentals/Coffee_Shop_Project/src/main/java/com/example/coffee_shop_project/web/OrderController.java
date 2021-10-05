66931cde387dd15265f8b5e343aae8f28d938225
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-05 19:05:27 +0300

    Order controller post mapping

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/OrderController.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/OrderController.java
@@ -1,8 +1,14 @@
 package com.example.coffee_shop_project.web;
 
+import com.example.coffee_shop_project.models.biding.OrderBindingModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/orders")
@@ -12,5 +18,26 @@ public class OrderController {
     public String add() {
 
         return "order-add";
+    }
+
+
+
+    @PostMapping("/order-add")
+    public String addConfirm(@Valid OrderBindingModel orderBindingModel,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) {
+
+
+        if (bindingResult.hasErrors()) {
+
+            redirectAttributes.addFlashAttribute("orderBindingModel", orderBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.orderBindingModel", bindingResult);
+
+
+
+        }
+
+
+
     }
 }
