1c070af9cf4b1696e4217ccf8b136db1f15ad623
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-05 18:46:56 +0300

    Order controller

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/OrderServiceImpl.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.coffee_shop_project.service.Impl;
 
 import com.example.coffee_shop_project.service.OrderService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderServiceImpl implements OrderService {
 }
