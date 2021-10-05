ad07c9cd2ebfeb0bfc7b9e424332551d13928f07
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-05 18:41:27 +0300

    Order binding model validating fields

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/biding/OrderBindingModel.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/biding/OrderBindingModel.java
@@ -2,7 +2,9 @@ package com.example.coffee_shop_project.models.biding;
 
 import com.example.coffee_shop_project.models.entities.Category;
 import com.example.coffee_shop_project.models.entities.EnumCategory;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -14,4 +16,55 @@ public class OrderBindingModel {
     private LocalDateTime orderTime;
     private EnumCategory category;
     private String description;
+
+    public OrderBindingModel() {
+    }
+
+
+    @Size(min = 3, max = 20 , message = "Name must be between 3 and 20 characters")
+    @NotBlank
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @DecimalMin("0")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd:'T'HH:mm")
+    @PastOrPresent
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    @NotNull(message = "Select category!")
+    public EnumCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(EnumCategory category) {
+        this.category = category;
+    }
+
+    @Size(min = 5, message = "Description must be min 5 characters")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
