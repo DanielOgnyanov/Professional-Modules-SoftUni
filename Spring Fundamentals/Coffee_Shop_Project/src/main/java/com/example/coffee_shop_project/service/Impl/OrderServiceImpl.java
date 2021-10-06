28eab321c0dfffcd2574a64d3cd5cce304a03556
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-06 19:22:32 +0300

    Order service impl method to save

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/OrderServiceImpl.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.coffee_shop_project.service.Impl;
 
+import com.example.coffee_shop_project.models.entities.Order;
 import com.example.coffee_shop_project.models.service.OrderServiceModel;
 import com.example.coffee_shop_project.repository.OrderRepository;
+import com.example.coffee_shop_project.service.CategoryService;
 import com.example.coffee_shop_project.service.OrderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -10,16 +12,21 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final CategoryService categoryService;
     private final ModelMapper modelMapper;
 
-    public OrderServiceImpl(OrderRepository orderRepository, ModelMapper modelMapper) {
+    public OrderServiceImpl(OrderRepository orderRepository, CategoryService categoryService, ModelMapper modelMapper) {
         this.orderRepository = orderRepository;
+        this.categoryService = categoryService;
         this.modelMapper = modelMapper;
     }
 
 
     @Override
-    public void add(OrderServiceModel map) {
+    public void add(OrderServiceModel orderServiceModel) {
 
+        Order order = modelMapper.map(orderServiceModel, Order.class);
+
+        order.setCategory();
     }
 }
