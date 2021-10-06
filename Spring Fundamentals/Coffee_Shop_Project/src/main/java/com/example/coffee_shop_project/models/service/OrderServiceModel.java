181266822a8758d642ef1d3572f8f5a4a764c756
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-06 19:11:29 +0300

    Order service model

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/service/OrderServiceModel.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/service/OrderServiceModel.java
@@ -1,4 +1,18 @@
 package com.example.coffee_shop_project.models.service;
 
+import com.example.coffee_shop_project.models.entities.Category;
+import com.example.coffee_shop_project.models.entities.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class OrderServiceModel {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private LocalDateTime orderTime;
+    private Category category;
+    private String description;
+    private User employee;
 }
