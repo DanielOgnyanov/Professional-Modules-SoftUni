146052f0c524c9ec0621f6a55a81f358209f7659
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-06 19:18:54 +0300

    Order service impl method to save

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/OrderServiceImpl.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/OrderServiceImpl.java
@@ -1,11 +1,23 @@
 package com.example.coffee_shop_project.service.Impl;
 
 import com.example.coffee_shop_project.models.service.OrderServiceModel;
+import com.example.coffee_shop_project.repository.OrderRepository;
 import com.example.coffee_shop_project.service.OrderService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final ModelMapper modelMapper;
+
+    public OrderServiceImpl(OrderRepository orderRepository, ModelMapper modelMapper) {
+        this.orderRepository = orderRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
     @Override
     public void add(OrderServiceModel map) {
 
