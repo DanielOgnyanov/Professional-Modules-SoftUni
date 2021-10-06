be65b2fff1a55a07c3ca96b8065823d5f1cd122a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-06 20:53:25 +0300

    Order repo adding order

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/repository/UserRepository.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.coffee_shop_project.repository;
 
 import com.example.coffee_shop_project.models.entities.User;
+import com.example.coffee_shop_project.models.service.OrderServiceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsernameAndPassword(String username, String password);
 
+    Optional<User> findByUsername(String username);
 
 }
--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/OrderServiceImpl.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/service/Impl/OrderServiceImpl.java
@@ -5,23 +5,25 @@ import com.example.coffee_shop_project.models.service.OrderServiceModel;
 import com.example.coffee_shop_project.repository.OrderRepository;
 import com.example.coffee_shop_project.service.CategoryService;
 import com.example.coffee_shop_project.service.OrderService;
+import com.example.coffee_shop_project.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
     private final CategoryService categoryService;
+    private final UserService userService;
     private final ModelMapper modelMapper;
 
-    public OrderServiceImpl(OrderRepository orderRepository, CategoryService categoryService, ModelMapper modelMapper) {
+    public OrderServiceImpl(OrderRepository orderRepository, CategoryService categoryService, UserService userService, ModelMapper modelMapper) {
         this.orderRepository = orderRepository;
         this.categoryService = categoryService;
+        this.userService = userService;
         this.modelMapper = modelMapper;
     }
 
@@ -33,10 +35,13 @@ public class OrderServiceImpl implements OrderService {
 
         order.setCategory(categoryService.findByName(orderServiceModel.getCategory()));
 
+
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
         LocalDateTime localDateTime = LocalDateTime.parse(orderServiceModel.getOrderTime(), dtf);
 
         order.setOrderTime(localDateTime);
+
+
         orderRepository.save(order);
     }
 }
