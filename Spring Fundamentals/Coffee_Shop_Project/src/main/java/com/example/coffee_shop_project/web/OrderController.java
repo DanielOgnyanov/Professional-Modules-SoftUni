c40caf34525beb57f92eb4ad9e471168cc14546f
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-08 19:57:24 +0300

    Home html get id of the orders

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/OrderController.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -60,7 +61,15 @@ public class OrderController {
 
         return "redirect:/";
 
+    }
 
 
+
+    @GetMapping("/ready/{id}")
+    public String deleteOrderById(@PathVariable Long id) {
+
+        orderService.getReady(id);
+
+        return "redirect:/";
     }
 }
