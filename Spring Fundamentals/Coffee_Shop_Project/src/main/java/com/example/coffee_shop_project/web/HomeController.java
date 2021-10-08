3ae1527ae1ead9847b0d6d31884ad6ce5b6ccaaa
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-08 19:01:56 +0300

    Calc needed time to prepare the orders

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/HomeController.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.coffee_shop_project.web;
 
 
+import com.example.coffee_shop_project.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,20 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class HomeController {
 
+    private final OrderService orderService;
+
+    public HomeController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+
     @GetMapping("/")
     public String index(HttpSession httpSession, Model model) {
         if(httpSession.getAttribute("user") == null){
             return "index";
         }
+
+        model.addAttribute("totalTime");
 
         return "home";
     }
