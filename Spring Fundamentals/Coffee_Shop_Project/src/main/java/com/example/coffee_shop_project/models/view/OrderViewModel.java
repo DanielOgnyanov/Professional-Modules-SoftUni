124afda0c5bed15a8c688bb80005cc44abed317d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-08 19:24:58 +0300

    Order view model fields

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/view/OrderViewModel.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/view/OrderViewModel.java
@@ -8,4 +8,30 @@ public class OrderViewModel {
     private String price;
 
 
+    public OrderViewModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
