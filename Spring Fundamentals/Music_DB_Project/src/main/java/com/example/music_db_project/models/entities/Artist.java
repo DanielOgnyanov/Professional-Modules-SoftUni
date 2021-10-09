7c63e2d5d6e58257c9799ae859a5e2668e63c685
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-09 16:07:45 +0300

    Artist entiry

---
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/Artist.java
@@ -0,0 +1,9 @@
+package com.example.music_db_project.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "artists")
+public class Artist extends BaseEntity{
+}
