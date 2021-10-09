262a7beb44d3e2ff607f404e0f479fdc629cd0cc
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-09 16:19:37 +0300

    Genre enum.

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/Album.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/Album.java
@@ -3,12 +3,21 @@ package com.example.music_db_project.models.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "albums")
 public class Album extends BaseEntity{
 
-
+    private String name;
+    private String imageUrl;
+    private String description;
+    private int copies;
+    private BigDecimal price;
+    private LocalDate releasedDate;
+    private String producer;
+    private
 
 
 }
---
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/EnumGenre.java
@@ -0,0 +1,5 @@
+package com.example.music_db_project.models.entities;
+
+public enum EnumGenre {
+    POP, ROCK, METAL, OTHER;
+}
