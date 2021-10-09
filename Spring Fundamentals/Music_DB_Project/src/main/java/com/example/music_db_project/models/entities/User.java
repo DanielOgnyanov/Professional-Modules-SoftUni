ad2b6675763c0c976956b72014e3011588f6f8a5
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-09 16:05:32 +0300

    User entity fields

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/User.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/User.java
@@ -1,6 +1,7 @@
 package com.example.music_db_project.models.entities;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -14,5 +15,43 @@ public class User extends BaseEntity{
     private String email;
 
 
+    public User() {
+    }
 
+
+    @Column(name = "username", unique = true)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(name = "full_name")
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "email", unique = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
