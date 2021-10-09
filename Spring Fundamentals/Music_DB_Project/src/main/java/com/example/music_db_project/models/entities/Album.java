ff7b2b175c7f66e66cae40d0024b5a835d8801fc
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-09 16:15:19 +0300

    Album entity

---
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/Album.java
@@ -0,0 +1,14 @@
+package com.example.music_db_project.models.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "albums")
+public class Album extends BaseEntity{
+
+
+
+
+}
