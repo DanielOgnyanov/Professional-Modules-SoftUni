5af6727530ec3ead760923e14a5acd35e91a698a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-09 15:32:23 +0300

    Pom file set

---
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/MusicDbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_db_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicDbProjectApplication.class, args);
+    }
+
+}
---
+++ Spring Fundamentals/Music_DB_Project/src/test/java/com/example/music_db_project/MusicDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.music_db_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
