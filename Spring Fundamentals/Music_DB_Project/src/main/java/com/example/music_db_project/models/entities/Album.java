072a6960baa2e1c9ec674a44a7eb4eb1f5be5dfe
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-09 16:21:14 +0300

    Album entity fields.

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/Album.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/Album.java
@@ -17,7 +17,91 @@ public class Album extends BaseEntity{
     private BigDecimal price;
     private LocalDate releasedDate;
     private String producer;
-    private
+    private EnumGenre genre;
+    private Artist artist;
+    private User addedFrom;
 
 
+    public Album() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getReleasedDate() {
+        return releasedDate;
+    }
+
+    public void setReleasedDate(LocalDate releasedDate) {
+        this.releasedDate = releasedDate;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public EnumGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(EnumGenre genre) {
+        this.genre = genre;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public User getAddedFrom() {
+        return addedFrom;
+    }
+
+    public void setAddedFrom(User addedFrom) {
+        this.addedFrom = addedFrom;
+    }
 }
