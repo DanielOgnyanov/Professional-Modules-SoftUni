0ee923a4728bf411d8dd8b970f6ac40d6f716f48
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-09 15:53:28 +0300

    Base entity

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/BaseEntity.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/BaseEntity.java
@@ -1,4 +1,25 @@
 package com.example.music_db_project.models.entities;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class BaseEntity {
+
+    private Long id;
+
+    public BaseEntity() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false,unique = true)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
