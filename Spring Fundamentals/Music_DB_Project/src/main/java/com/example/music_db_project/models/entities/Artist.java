7aabdb66943e6900af53e00abaac38e420912d52
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-09 16:12:52 +0300

    Artist entity fields

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/Artist.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/Artist.java
@@ -7,6 +7,25 @@ import javax.persistence.Table;
 @Table(name = "artists")
 public class Artist extends BaseEntity{
 
-    private String name;
+    private EnumArtist name;
     private String description;
+
+    public Artist() {
+    }
+
+    public EnumArtist getName() {
+        return name;
+    }
+
+    public void setName(EnumArtist name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
