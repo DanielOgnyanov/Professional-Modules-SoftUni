e704bee404dd3dc2d4e053e62636d171ed0c671e
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-10 15:57:29 +0300

    Artist service insert all artist in db.

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/Impl/ArtistServiceImpl.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/Impl/ArtistServiceImpl.java
@@ -1,8 +1,15 @@
 package com.example.music_db_project.service.Impl;
 
+import com.example.music_db_project.repository.ArtistRepository;
 import com.example.music_db_project.service.ArtistService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArtistServiceImpl implements ArtistService {
+
+    private final ArtistRepository artistRepository;
+
+    public ArtistServiceImpl(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
 }
