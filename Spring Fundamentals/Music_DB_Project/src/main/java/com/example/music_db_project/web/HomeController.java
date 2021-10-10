e589baf0db79653818f75c825ef1cea16a5906a3
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-10 16:54:21 +0300

    Home controller

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/HomeController.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/HomeController.java
@@ -1,4 +1,22 @@
 package com.example.music_db_project.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class HomeController {
+
+
+    @GetMapping("/")
+    public String index(HttpSession httpSession, Model model) {
+        if (httpSession.getAttribute("user") == null) {
+            return "index";
+        }
+        return "index";
+
+
+    }
 }
