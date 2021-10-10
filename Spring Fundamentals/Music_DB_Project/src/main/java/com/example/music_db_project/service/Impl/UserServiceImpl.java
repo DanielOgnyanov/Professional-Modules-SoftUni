6864a52bee13439691289115d09c8ea9c3629769
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-10 15:54:37 +0300

    User service impl

---
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/Impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.music_db_project.service.Impl;
+
+import com.example.music_db_project.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
