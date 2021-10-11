8e801a589e74a60f306673555ef66a2d0a4aa99f
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-11 18:21:53 +0300

    User controller

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/UserController.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/UserController.java
@@ -2,7 +2,11 @@ package com.example.music_db_project.web;
 
 import com.example.music_db_project.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
