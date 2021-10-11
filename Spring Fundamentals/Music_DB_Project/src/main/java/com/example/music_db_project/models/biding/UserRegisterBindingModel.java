8be4f481f0b1cbb4c0d75447c164949feebb031e
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-11 18:11:32 +0300

    User register binding model fields

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/biding/UserRegisterBindingModel.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/biding/UserRegisterBindingModel.java
@@ -1,4 +1,11 @@
 package com.example.music_db_project.models.biding;
 
 public class UserRegisterBindingModel {
+
+
+    private String username;
+    private String fullName;
+    private String email;
+    private String password;
+    private String confirmPassword;
 }
