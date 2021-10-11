ca71bee9fd8e20e0c0758edb5a80b5ff37481cb3
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-11 19:24:18 +0300

    user service model

---
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/UserServiceModel.java
@@ -0,0 +1,10 @@
+package com.example.music_db_project.models;
+
+public class UserServiceModel {
+
+    private Long id;
+    private String username;
+    private String fullName;
+    private String password;
+    private String email;
+}
