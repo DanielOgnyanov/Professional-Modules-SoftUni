188eff6e593897bbac5e26270e9f8a6dc92cb905
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-11 19:30:39 +0300

    user register method in service to insert into db the user.

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/Impl/UserServiceImpl.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/Impl/UserServiceImpl.java
@@ -1,8 +1,27 @@
 package com.example.music_db_project.service.Impl;
 
+import com.example.music_db_project.models.UserServiceModel;
+import com.example.music_db_project.repository.UserRepository;
 import com.example.music_db_project.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public void register(UserServiceModel map) {
+
+    }
 }
