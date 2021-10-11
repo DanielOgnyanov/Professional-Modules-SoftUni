40c5cc6db5edb868bce46d15e3a9a4e2f3f704ca
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-11 18:50:08 +0300

    User controller get mapping

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/UserController.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/UserController.java
@@ -1,8 +1,11 @@
 package com.example.music_db_project.web;
 
+import com.example.music_db_project.models.biding.UserRegisterBindingModel;
 import com.example.music_db_project.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +19,15 @@ public class UserController {
     public UserController(UserService userService, ModelMapper modelMapper) {
         this.userService = userService;
         this.modelMapper = modelMapper;
+    }
+
+
+    @GetMapping("/register")
+    public String register(Model model) {
+
+        if (!model.containsAttribute("userRegisterBindingModel")) {
+            model.addAttribute("userRegisterBindingModel", new UserRegisterBindingModel());
+        }
+        return "register";
     }
 }
