7a5ca21bf3b3589c33610f9322a6f56e644ae7b5
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-13 18:22:01 +0300

    album controller get mapping

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/AlbumController.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/AlbumController.java
@@ -1,9 +1,27 @@
 package com.example.music_db_project.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/albums")
 public class AlbumController {
+
+
+    @GetMapping("/add-album")
+    public String add(Model model, HttpSession httpSession) {
+
+        if (httpSession.getAttribute("user") == null) {
+            return "redirect:/login";
+        }
+
+        if (!model.containsAttribute("orderBindingModel")) {
+            model.addAttribute("orderBindingModel", );
+        }
+        return "add-album";
+    }
 }
