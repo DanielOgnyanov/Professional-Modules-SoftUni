8d265d66f7fae7f6b34327c5baf8b7bfccd124e2
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-13 19:28:58 +0300

    album service model

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/service/AlbumServiceModel.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/service/AlbumServiceModel.java
@@ -1,4 +1,114 @@
 package com.example.music_db_project.models.service;
 
+import com.example.music_db_project.models.entities.Artist;
+import com.example.music_db_project.models.entities.EnumGenre;
+import com.example.music_db_project.models.entities.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class AlbumServiceModel {
+
+    private Long id;
+    private String name;
+    private String imageUrl;
+    private String description;
+    private int copies;
+    private BigDecimal price;
+    private LocalDate releasedDate;
+    private String producer;
+    private EnumGenre genre;
+    private Artist artist;
+    private User addedFrom;
+
+    public AlbumServiceModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getReleasedDate() {
+        return releasedDate;
+    }
+
+    public void setReleasedDate(LocalDate releasedDate) {
+        this.releasedDate = releasedDate;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public EnumGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(EnumGenre genre) {
+        this.genre = genre;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public User getAddedFrom() {
+        return addedFrom;
+    }
+
+    public void setAddedFrom(User addedFrom) {
+        this.addedFrom = addedFrom;
+    }
 }
