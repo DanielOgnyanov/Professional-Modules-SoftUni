74ef924da291d337226cf4d56383ee0173035cc8
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-13 18:34:55 +0300

    album binding model fields

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/biding/AlbumBindingModel.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/biding/AlbumBindingModel.java
@@ -8,7 +8,6 @@ import java.time.LocalDate;
 
 public class AlbumBindingModel {
 
-    private Long id;
     private String name;
     private String imageUrl;
     private BigDecimal price;
@@ -20,6 +19,79 @@ public class AlbumBindingModel {
     private String description;
 
 
+    public AlbumBindingModel() {
+    }
 
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public LocalDate getReleasedDate() {
+        return releasedDate;
+    }
+
+    public void setReleasedDate(LocalDate releasedDate) {
+        this.releasedDate = releasedDate;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public EnumArtist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(EnumArtist artist) {
+        this.artist = artist;
+    }
+
+    public EnumGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(EnumGenre genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
