7505fe53dbdbcb937cb7017e3118c1ac77b912e9
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-13 18:32:15 +0300

    album binding model fields

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/biding/AlbumBindingModel.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/biding/AlbumBindingModel.java
@@ -1,4 +1,25 @@
 package com.example.music_db_project.models.biding;
 
+import com.example.music_db_project.models.entities.EnumArtist;
+import com.example.music_db_project.models.entities.EnumGenre;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class AlbumBindingModel {
+
+    private Long id;
+    private String name;
+    private String imageUrl;
+    private BigDecimal price;
+    private int copies;
+    private LocalDate releasedDate;
+    private String producer;
+    private EnumArtist artist;
+    private EnumGenre genre;
+    private String description;
+
+
+
+
 }
