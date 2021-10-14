c7ba30284f045f71bc8e09afa6e19d9e56bddbe2
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-14 19:13:44 +0300

    Current user component

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/CurrentUser.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/CurrentUser.java
@@ -10,5 +10,22 @@ public class CurrentUser {
     private Long id;
     private String username;
 
+    public CurrentUser() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
