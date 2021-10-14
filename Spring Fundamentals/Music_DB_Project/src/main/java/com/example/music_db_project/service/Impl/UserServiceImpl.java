8a89b7a35616529ce662896b31b63ca116802c2e
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-14 19:38:19 +0300

    Current user component

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/Impl/UserServiceImpl.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/Impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.music_db_project.service.Impl;
 
+import com.example.music_db_project.models.entities.CurrentUser;
 import com.example.music_db_project.models.service.UserServiceModel;
 import com.example.music_db_project.models.entities.User;
 import com.example.music_db_project.repository.UserRepository;
@@ -13,11 +14,13 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final CurrentUser currentUser;
 
 
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, CurrentUser currentUser) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.currentUser = currentUser;
     }
 
 
@@ -31,5 +34,10 @@ public class UserServiceImpl implements UserService {
         return userRepository.findByUsernameAndPassword(username,password)
                 .map(user -> modelMapper.map(user, UserServiceModel.class))
                 .orElse(null);
+    }
+
+    @Override
+    public void login(UserServiceModel userServiceModel) {
+
     }
 }
