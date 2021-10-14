a80dd309b98ab5d218561ead3d3d1d12f0f4dbbe
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-14 19:12:18 +0300

    Current user component

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/CurrentUser.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/entities/CurrentUser.java
@@ -1,4 +1,14 @@
 package com.example.music_db_project.models.entities;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
 public class CurrentUser {
+
+    private Long id;
+    private String username;
+
+
 }
