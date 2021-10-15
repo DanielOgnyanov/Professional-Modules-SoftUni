e2c68a84a358991c35114b55fa482d8434406c62
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-15 19:42:56 +0300

    album service impl method to find sold copies

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/AlbumService.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/service/AlbumService.java
@@ -1,9 +1,12 @@
 package com.example.music_db_project.service;
 
 import com.example.music_db_project.models.service.AlbumServiceModel;
+import com.example.music_db_project.models.view.AlbumViewModel;
+
+import java.util.List;
 
 public interface AlbumService {
     void add(AlbumServiceModel albumServiceModel);
 
-
+    List<AlbumViewModel> findAllSoldCopies();
 }
