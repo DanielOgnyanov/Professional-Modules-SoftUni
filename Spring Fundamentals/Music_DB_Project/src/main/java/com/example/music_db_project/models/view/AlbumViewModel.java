33e35d0da3c2c461dd7d9cdf26f5cc8859abebf2
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-15 19:27:08 +0300

    Album view model

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/view/AlbumViewModel.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/view/AlbumViewModel.java
@@ -1,4 +1,18 @@
 package com.example.music_db_project.models.view;
 
+import com.example.music_db_project.models.entities.Artist;
+import com.example.music_db_project.models.entities.EnumGenre;
+import com.example.music_db_project.models.entities.User;
+
+import java.math.BigDecimal;
+
 public class AlbumViewModel {
+
+    private Long id;
+    private String name;
+    private int copies;
+    private BigDecimal price;
+    private EnumGenre genre;
+    private Artist artist;
+    private User addedFrom;
 }
