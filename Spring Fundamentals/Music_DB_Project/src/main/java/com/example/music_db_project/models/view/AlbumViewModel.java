32bf49cb746396b63f603ee25af9ef0c9d48b924
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-15 19:30:24 +0300

    Album view model

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/view/AlbumViewModel.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/models/view/AlbumViewModel.java
@@ -15,4 +15,64 @@ public class AlbumViewModel {
     private EnumGenre genre;
     private Artist artist;
     private User addedFrom;
+
+
+    public AlbumViewModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public EnumGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(EnumGenre genre) {
+        this.genre = genre;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public User getAddedFrom() {
+        return addedFrom;
+    }
+
+    public void setAddedFrom(User addedFrom) {
+        this.addedFrom = addedFrom;
+    }
 }
