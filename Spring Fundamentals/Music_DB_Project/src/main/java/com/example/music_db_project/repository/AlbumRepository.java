4b513d349304f7048f8e1a8c3e1439b06093a536
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-15 19:36:55 +0300

    repo method to find all sold album

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/repository/AlbumRepository.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/repository/AlbumRepository.java
@@ -3,8 +3,14 @@ package com.example.music_db_project.repository;
 import com.example.music_db_project.models.entities.Album;
 import com.example.music_db_project.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+    @Query("Select SUM(a.copies) From Album as a")
+    BigDecimal allSoldCopies();
 }
