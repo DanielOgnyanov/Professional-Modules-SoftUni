9a467a851b3565da9fa56519eaed3252c15bd785
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-16 16:05:42 +0300

    home controller add model

--- Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/HomeController.java
+++ Spring Fundamentals/Music_DB_Project/src/main/java/com/example/music_db_project/web/HomeController.java
@@ -1,20 +1,26 @@
 package com.example.music_db_project.web;
 
+import com.example.music_db_project.models.view.AlbumViewModel;
 import com.example.music_db_project.security.CurrentUser;
 import com.example.music_db_project.service.AlbumService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
 
     private final CurrentUser currentUser;
     private final AlbumService albumService;
+    private final ModelMapper modelMapper;
 
-    public HomeController(CurrentUser currentUser, AlbumService albumService) {
+    public HomeController(CurrentUser currentUser, AlbumService albumService, ModelMapper modelMapper) {
         this.currentUser = currentUser;
         this.albumService = albumService;
+        this.modelMapper = modelMapper;
     }
 
 
@@ -25,6 +31,9 @@ public class HomeController {
         }
 
         model.addAttribute("totalSoldCopies", albumService.findAllSoldCopies());
+        model.addAttribute("albums", albumService.getAllOrdered().stream()
+                .map(a -> modelMapper
+                        .map(a, AlbumViewModel.class)).collect(Collectors.toList()));
         return "home";
 
 
