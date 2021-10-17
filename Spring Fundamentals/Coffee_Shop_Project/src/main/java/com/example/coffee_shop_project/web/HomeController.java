51b6928c666aa646a2c961fc24c0baf1404366ce
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-17 15:57:39 +0300

    home controller change http with current user class

--- Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/HomeController.java
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/web/HomeController.java
@@ -2,26 +2,27 @@ package com.example.coffee_shop_project.web;
 
 
 import com.example.coffee_shop_project.models.entities.EnumCategory;
+import com.example.coffee_shop_project.security.CurrentUser;
 import com.example.coffee_shop_project.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 @Controller
 public class HomeController {
 
     private final OrderService orderService;
+    private final CurrentUser currentUser;
 
-    public HomeController(OrderService orderService) {
+    public HomeController(OrderService orderService, CurrentUser currentUser) {
         this.orderService = orderService;
+        this.currentUser = currentUser;
     }
 
 
     @GetMapping("/")
-    public String index(HttpSession httpSession, Model model) {
-        if(httpSession.getAttribute("user") == null){
+    public String index(Model model) {
+        if(currentUser.isAnonymous()){
             return "index";
         }
 
