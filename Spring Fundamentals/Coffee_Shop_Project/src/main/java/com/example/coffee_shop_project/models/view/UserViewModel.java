c5e2971e94cfb4b2c840097ec1c5c91353231233
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-17 19:33:38 +0300

    logout implement

---
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/models/view/UserViewModel.java
@@ -0,0 +1,28 @@
+package com.example.coffee_shop_project.models.view;
+
+public class UserViewModel {
+
+    private Long count;
+    private String username;
+
+
+    public UserViewModel() {
+    }
+
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
