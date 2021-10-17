f8c8a314f6dfdff1178da39a09c2bf06ff79341b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-10-17 15:48:13 +0300

    Create current user class to change the access to the logged user.

---
+++ Spring Fundamentals/Coffee_Shop_Project/src/main/java/com/example/coffee_shop_project/security/CurrentUser.java
@@ -0,0 +1,9 @@
+package com.example.coffee_shop_project.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class CurrentUser {
+}
